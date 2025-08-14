1c9e03aaf1ff255e86bb6d964fcc893c5ceffa88
Runny <xiaoqiang9609@gmail.com>
2025-08-14 17:41:00 +0800

    Update formula with real SHA256 for v1.0.0

--- Formula/pasteimage.rb
+++ Formula/pasteimage.rb
@@ -2,7 +2,7 @@ class Pasteimage < Formula
   desc "Fast clipboard image saving for macOS"
   homepage "https://github.com/RunnyC/clipboard-image-saver"
   url "https://github.com/RunnyC/clipboard-image-saver/releases/download/v1.0.0/pasteimage-macos.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "ab3003b4985c5a242d6a46fd6085fd983f87ba844b3026ab379d20493c569c3e"
   license "MIT"
 
   depends_on :macos

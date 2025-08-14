32923bce044df27c5b29ea5447c7b86db0488630
Runny <xiaoqiang9609@gmail.com>
2025-08-14 17:15:00 +0800

    Fix formula syntax and GitHub Actions cache version

--- Formula/pasteimage.rb
+++ Formula/pasteimage.rb
@@ -2,7 +2,7 @@ class Pasteimage < Formula
   desc "Fast clipboard image saving for macOS"
   homepage "https://github.com/RunnyC/clipboard-image-saver"
   url "https://github.com/RunnyC/clipboard-image-saver/releases/download/v1.0.0/pasteimage-macos.tar.gz"
-  sha256 "PLACEHOLDER_SHA256_UPDATE_AFTER_BUILD"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "MIT"
 
   depends_on :macos
@@ -12,6 +12,6 @@ class Pasteimage < Formula
   end
 
   test do
-    system "#{bin}/pasteimage --help"
+    system "#{bin}/pasteimage", "--help"
   end
 end

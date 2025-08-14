74fa5419c63312651a6ee310343cf51f66b21ba2
Runny <xiaoqiang9609@gmail.com>
2025-08-14 17:08:44 +0800

    Fix formula structure

--- Formula/pasteimage.rb
+++ Formula/pasteimage.rb
@@ -2,7 +2,7 @@ class Pasteimage < Formula
   desc "Fast clipboard image saving for macOS"
   homepage "https://github.com/RunnyC/clipboard-image-saver"
   url "https://github.com/RunnyC/clipboard-image-saver/releases/download/v1.0.0/pasteimage-macos.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "PLACEHOLDER_SHA256_UPDATE_AFTER_BUILD"
   license "MIT"
 
   depends_on :macos
@@ -12,6 +12,6 @@ class Pasteimage < Formula
   end
 
   test do
-    assert_match "Usage", shell_output("#{bin}/pasteimage --help 2>&1")
+    system "#{bin}/pasteimage --help"
   end
 end

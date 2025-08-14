324657a2c93cce8d3dc3736fec3868008f9085a5
Runny <xiaoqiang9609@gmail.com>
2025-08-15 01:47:56 +0800

    feat: update pasteimage to v1.1.0
    - Update to latest release v1.1.0
    - Add --version flag support
    - Update SHA256 to match release asset

--- Formula/pasteimage.rb
+++ Formula/pasteimage.rb
@@ -1,8 +1,8 @@
 class Pasteimage < Formula
   desc "Fast clipboard image saving for macOS"
   homepage "https://github.com/RunnyC/clipboard-image-saver"
-  url "https://github.com/RunnyC/clipboard-image-saver/releases/download/v1.0.2/pasteimage-macos.tar.gz"
-  sha256 "8366b3c09f03dc3c0cf75293dfd28e56ba9785b43b4f5f352cb0f613cfbbdb03"
+  url "https://github.com/RunnyC/clipboard-image-saver/releases/download/v1.1.0/pasteimage-macos.tar.gz"
+  sha256 "4648ae53a393ce6fd967604606ce88858a116f4290b0296f6d434e56b5f24a4c"
   license "MIT"
 
   depends_on :macos
@@ -13,5 +13,6 @@ class Pasteimage < Formula
 
   test do
     system bin/"pasteimage", "--help"
+    system bin/"pasteimage", "--version"
   end
 end
